d2504ba2f89cd716dca66e7f344f901d71cd59d3
Bayan999 <b-nayf@hotmail.com>
2023-10-09 10:27:20 +0300

    Initial Commit

---
+++ ooo/oooApp.swift
@@ -0,0 +1,17 @@
+//
+//  oooApp.swift
+//  ooo
+//
+//  Created by Bayan on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct oooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
